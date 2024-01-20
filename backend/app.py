58d6942f65b606ec9fb90128f1b3fe922d30c123
Jacob Rees <reescodes@gmail.com>
2024-01-20 13:29:39 -0700

    add second route for snapchat data

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,10 @@ def hello():
     result = main.get_data("TSLA")
     return result
 
-
+@app.route('/snap')
+def snap_data():
+    result = main.get_data("SNAP")
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
